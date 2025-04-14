fbb6f4692454e6b530a709cc52bcd3501cd2d419
Heclypso <heclypso@gmail.com>
2025-04-14 11:36:02 -0300

    #1 feat: adicionado cors ao código

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
+import cors from 'cors'
 import express from 'express'
 const app = express()
-// const PORT = 3000;
+const PORT = 3000;
+
+app.use(cors())
 
 const informacoesDeAcesso = {
     sistemaOperacional: 'Kali NetHunter',
@@ -12,6 +15,6 @@ app.get('/api/falso-acesso', (req, res) => {
     res.send(informacoesDeAcesso)
 })
 
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando em http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/api/falso-acesso`);
+});
