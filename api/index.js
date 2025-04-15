65d8b96eab0ad419e3652d3f97b0bd5254589af0
Heclypso <heclypso@gmail.com>
2025-04-15 09:10:10 -0300

    #3 refactor: criado serverless function para a versel, commitado código para uso local

--- api/index.js
+++ api/index.js
@@ -1,20 +1,24 @@
-import cors from 'cors'
-import express from 'express'
-const app = express()
-const PORT = 3000;
+// import cors from 'cors'
+// import express from 'express'
+// const app = express()
+// const PORT = 3000;
 
-app.use(cors())
+// app.use(cors())
 
-const informacoesDeAcesso = {
-    sistemaOperacional: 'Kali NetHunter',
-    local: 'Chengdu, China',
-    navegador: 'Safari', 
+export default function handler(req, res) {
+    const informacoesDeAcesso = {
+        sistemaOperacional: 'Kali NetHunter',
+        local: 'Chengdu, China',
+        navegador: 'Safari', 
+    }
+
+    res.status(200).json(informacoesDeAcesso)
 }
 
-app.get('/api/falso-acesso', (req, res) => {
-    res.send(informacoesDeAcesso)
-})
+// app.get('/api/falso-acesso', (req, res) => {
+//     res.send(informacoesDeAcesso)
+// })
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}/api/falso-acesso`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando em http://localhost:${PORT}/api/falso-acesso`);
+// });
